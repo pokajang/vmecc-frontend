f81b3f2e838be786e307f46b5e9c1cff9f0ecf7a
Azam Husain <azamhusain9@gmail.com>
2026-07-02 10:49:16 +0800

    chore(frontend): separate production build artifact refresh

---
+++ build/assets/RowActionCell-nT39ibzq.js
@@ -0,0 +1,1 @@
+import{n as s}from"./index-DJICDi7P.js";import{e as a}from"./CTable-BLrOFjxX.js";const t=o=>{o.stopPropagation()},p=({children:o,className:n="text-center",...e})=>s.jsx(a,{...e,className:n,onClick:t,onMouseDown:t,onKeyDown:t,children:o});export{p as R};
